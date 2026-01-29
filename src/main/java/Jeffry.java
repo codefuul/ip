955d720f684cf40dfa91fb51ea225e9ed6295454
leonardlim2411-admin <leonardlim2411@gmail.com>
2026-01-29 22:34:51 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Jeffry.java
+++ src/main/java/Jeffry.java
@@ -1,29 +1,52 @@
+import java.util.Scanner;
+
 public class Jeffry {
     public static void main(String[] args) {
+        // 1. Define the maximum number of tasks (e.g., 100)
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String logo = "   _           __    __          _   _ \n"
                 + "  (_)   ___   / _|  / _|  _ __  | | | |\n"
                 + "  | |  / _ \\ | |_  | |_  | '__| | |_| |\n"
                 + " _/ | |  __/ |  _| |  _| | |     \\__, |\n"
                 + "|__/   \\___| |_|   |_|   |_|     |___/ \n";
-        System.out.println("Hello from\n" + logo);
 
-        System.out.println("--------------------------------------------------");
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Jeffry.");
         System.out.println("What can I do for you?");
-        System.out.println("--------------------------------------------------");
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
         String userInput;
+
         while (true) {
             userInput = scanner.nextLine();
+
             if (userInput.equals("bye")) {
                 break;
+
+            } else if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+
+            } else {
+                // ADD command: Store the input in the array
+                tasks[taskCount] = userInput;
+                taskCount++;
+
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("--------------------------------------------------");
-            System.out.println(userInput);
-            System.out.println("--------------------------------------------------");
         }
-        System.out.println("--------------------------------------------------");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("--------------------------------------------------");
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }

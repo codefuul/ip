457f9d813994d56de760bcb05b5c49dbe58780e5
leonardlim2411-admin <leonardlim2411@gmail.com>
2026-01-29 23:08:49 +0800

    Add the ability to mark tasks as done and add the ability to change the status back to not done.

--- src/main/java/Jeffry.java
+++ src/main/java/Jeffry.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Jeffry {
     public static void main(String[] args) {
-        // 1. Define the maximum number of tasks (e.g., 100)
-        String[] tasks = new String[100];
+        // Change array type from String[] to Task[]
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         String logo = "   _           __    __          _   _ \n"
@@ -23,29 +23,49 @@ public class Jeffry {
 
         while (true) {
             userInput = scanner.nextLine();
+            System.out.println("____________________________________________________________");
 
             if (userInput.equals("bye")) {
                 break;
 
             } else if (userInput.equals("list")) {
-                System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    // Uses the toString() method we defined in Task class
+                    System.out.println((i + 1) + "." + tasks[i].toString());
                 }
-                System.out.println("____________________________________________________________");
+
+            } else if (userInput.startsWith("mark")) {
+                // Parse "mark 2" -> get integer 2
+                String[] parts = userInput.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1; // Convert 1-based to 0-based index
+
+                tasks[taskIndex].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex].toString());
+
+            } else if (userInput.startsWith("unmark")) {
+                // Parse "unmark 2" -> get integer 2
+                String[] parts = userInput.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+
+                tasks[taskIndex].markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex].toString());
 
             } else {
-                // ADD command: Store the input in the array
-                tasks[taskCount] = userInput;
+                // Default: Add a new task
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
 
-                System.out.println("____________________________________________________________");
                 System.out.println("added: " + userInput);
-                System.out.println("____________________________________________________________");
             }
+
+            System.out.println("____________________________________________________________");
         }
 
-        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
